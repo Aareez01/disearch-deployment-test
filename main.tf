e1d0bbb213e28a0a8922e13cee951f64fe19ceca
Nabeel Hassan <thenabeelhassan@gmail.com>
2023-09-23 14:38:36 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 # Define a variable using the environment variable directly
-variable "tf_var_name" {
+variable "projectName" {
   type = string
 }
 
-output "tf_var_value" {
-  value = var.tf_var_name
+output "project" {
+  value = var.projectName
 }
 
