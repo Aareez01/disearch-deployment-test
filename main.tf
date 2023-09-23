9384289eb36e7dbb593156519a7eb00243db8ad3
Aareez01 <124786944+Aareez01@users.noreply.github.com>
2023-09-23 13:49:59 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "google" {
+  credentials = file(var.google_credentials_file)
+  project    = var.project_id
+  region     = var.region
+}
+
 variable "projectName" {
   description = "Name of the Project"
   type        = string
