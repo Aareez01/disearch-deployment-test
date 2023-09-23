c1f3784bfd96a5a52be08e31a8263e24eba46500
Nabeel Hassan <thenabeelhassan@gmail.com>
2023-09-23 14:17:53 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
+# Define a variable using the environment variable directly
 variable "projectName" {
   type    = string
-  default = "${var.projectName}"
+  default = var.TF_VAR_projectName
 }
 
 output "project" {
